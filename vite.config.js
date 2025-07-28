d7bc19e8161d2c39306994fee52071bd764e53cf
VenkatramanG45 <venkatramang23092003@gmail.com>
2025-07-28 22:24:19 +0530

    Update Workflows

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { Buffer } from "buffer";
 
 export default defineConfig({
   plugins: [react()],
   base: "./", // Ensures proper relative paths for deployment
-  define: {
-    global: "globalThis",
-    Buffer: "globalThis.Buffer",
-  },
-  resolve: {
-    alias: {
-      crypto: "crypto-browserify", // Ensure crypto is properly aliased
-    },
-  },
 });
