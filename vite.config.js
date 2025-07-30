55b67f3de82ca45003d5f400cc870ac70a9da135
VenkatramanG45 <venkatramang23092003@gmail.com>
2025-07-30 11:26:46 +0530

    Created Staging

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./", // Ensures proper relative paths for deployment
-  build: {
-    rollupOptions: {
-      external: ["react-router-dom"], // Externalize react-router-dom
-    },
-  },
 });
