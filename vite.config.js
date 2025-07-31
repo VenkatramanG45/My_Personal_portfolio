8839e9491322c7f9447b197887e293bf2d748ce9
VenkatramanG45 <venkatramang23092003@gmail.com>
2025-07-31 10:50:41 +0530

    Added Layout

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   base: "./", // Ensures proper relative paths for deployment
+  optimizeDeps: {
+    include: [
+      'react-tilt',
+      'react-vertical-timeline-component',
+      '@emailjs/browser',
+      'maath/random/dist/maath-random.esm'
+    ]
+  }
 });
