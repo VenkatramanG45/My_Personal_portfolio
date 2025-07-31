ba18695194536fc691264de93aec3a08a1fef98e
VenkatramanG45 <venkatramang23092003@gmail.com>
2025-07-31 20:56:37 +0530

    Made_UI_Changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,8 +4,8 @@ import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
-import menu from "./menu.svg";
-import close from "./close.svg";
+import { ReactComponent as menu } from "./menu.svg";
+import { ReactComponent as close } from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -19,7 +19,7 @@ import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import { ReactComponent as threejs } from "./tech/threejs.svg";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
