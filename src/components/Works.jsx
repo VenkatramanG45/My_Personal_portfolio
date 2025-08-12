7347c0ef5790b2fe9123d9a9afd8e867394e894f
VenkatramanG45 <venkatramang23092003@gmail.com>
2025-08-12 18:49:48 +0530

    Mobile View

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,101 +1,5 @@
-import React from "react";
-import Tilt from "react-parallax-tilt";
-import { motion } from "framer-motion";
-import { useNavigate } from "react-router-dom";
-
-import { styles } from "../styles";
-import { github, web } from "../assets";
-import { SectionWrapper } from "../hoc";
-import { projects } from "../constants";
-import { fadeIn, textVariant } from "../utils/motion";
-
-const ProjectCard = ({
-  index,
-  name,
-  description,
-  tags,
-  image,
-  source_code_link,
-  website_url,
-}) => {
-  const navigate = useNavigate();
-
-  const handleCardClick = () => {
-    navigate(`/project/${index}`);
-  };
-
-  return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        className='bg-tertiary p-5 rounded-2xl min-h-[400px] h-full overflow-hidden group'
-        tiltMaxAngleX={45}
-        tiltMaxAngleY={45}
-        scale={1}
-        transitionSpeed={450}
-        onClick={handleCardClick}
-      >
-        <div className='relative w-full h-[230px]'>
-          <img
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
-          />
-
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
-            >
-              <img
-                src={github}
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
-              />
-            </div>
-            {website_url && (
-              <div
-                onClick={() => window.open(website_url, "_blank")}
-                className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
-              >
-                <img
-                  src={web}
-                  alt='website'
-                  className='w-1/2 h-1/2 object-contain'
-                />
-              </div>
-            )}
-          </div>
-        </div>
-
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <div className='mt-2 h-[70px] overflow-hidden'>
-            <p className='text-secondary text-[14px]'>{description}</p>
-          </div>
-        </div>
-
-        <div className='mt-4 flex flex-wrap gap-2'>
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
-            </p>
-          ))}
-        </div>
-        <div className='mt-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
-          <div
-            className="text-white font-bold text-[18px] underline cursor-pointer"
-            onClick={handleCardClick}
-          >
-            View More
-          </div>
-        </div>
-      </Tilt>
-    </motion.div>
-  );
-};
+import ProjectCard from "./ProjectCard";
+import Contact from "./Contact";
 
 const Works = () => {
   return (
@@ -118,10 +22,13 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className='mt-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7'>
+      <div className='mt-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 hidden sm:grid'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
+      </div>
+      <div className="block sm:hidden mt-20">
+        <Contact />
       </div>
     </>
   );
